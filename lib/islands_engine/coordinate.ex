79c1bc8aee94bad5893a27282d6d060346e4c112
Onorio Catenacci <catenacci@ieee.org>
2018-01-26 12:43:02 -0500

    Adding new file
    Starting on the islands_engine exercise

---
+++ lib/islands_engine/coordinate.ex
@@ -0,0 +1,11 @@
+defmodule IslandsEngine.Coordinate do
+  alias __MODULE__
+
+  @enforce_keys [:row, :col]
+  defstruct [:row, :col]
+
+  @bounds 1..10
+
+  def new(row, col) when row in @bounds and col in @bounds, do: %Coordinate{row: row, col: col}
+  def new(_row, _col), do: {:error, :invalid_coordinates}
+end
