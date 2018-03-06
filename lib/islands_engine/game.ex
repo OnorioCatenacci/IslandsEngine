23d60d2784c1fff5df531fe68808d8e2ba03dc27
Onorio Catenacci <catenacci@ieee.org>
2018-03-06 12:54:51 -0500

    Starting to implement game genserver

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -1,20 +1,12 @@
 defmodule IslandsEngine.Game do
   use GenServer
+  alias IslandsEngine.{Board, Guesses, Rules}
 
-  def handle_info(:first, state) do
-    IO.puts("This message has been handled by handle_info/2, matching on :first")
-    {:noreply, state}
-  end
+  def start_link(name) when is_binary(name), do: GenServer.start_link(__MODULE__, name, [])
 
-  def handle_call(:demo_call, _from, state) do
-    {:reply, state, state}
-  end
-
-  def demo_call(game) do
-    GenServer.call(game, :demo_call)
-  end
-
-  def handle_cast({:demo_cast, new_value}, state) do
-    {:noreply, Map.put(state, :test, new_value)}
+  def init(name) do
+    player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
+    player2 = %{name: nil, board: Board.new(), guesses: Guesses.new()}
+    {:ok, %{player1: player1, player2: player2, rules: %Rules{}}}
   end
 end
