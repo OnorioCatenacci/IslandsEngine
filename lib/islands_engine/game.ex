3319c607db405f11a392d7430b5096cce7e71851
Onorio Catenacci <catenacci@ieee.org>
2018-03-26 12:54:48 -0400

    End of Part 2
    The code at the end of Part 2 of Functional Web Development.

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -8,9 +8,8 @@ defmodule IslandsEngine.Game do
     do: GenServer.start_link(__MODULE__, name, name: via_tuple(name))
 
   def init(name) do
-    player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
-    player2 = %{name: nil, board: Board.new(), guesses: Guesses.new()}
-    {:ok, %{player1: player1, player2: player2, rules: %Rules{}}, @timeout}
+    send(self(), {:set_state, name})
+    {:ok, fresh_state(name)}
   end
 
   def add_player(game, name) when is_binary(name), do: GenServer.call(game, {:add_player, name})
@@ -90,6 +89,24 @@ defmodule IslandsEngine.Game do
     {:stop, {:shutdown, :timeout}, state_data}
   end
 
+  def handle_info({:set_state, name}, _state_data) do
+    state_data =
+      case :ets.lookup(:game_state, name) do
+        [] -> fresh_state(name)
+        [{_key, state}] -> state
+      end
+
+    :ets.insert(:game_state, {name, state_data})
+    {:noreply, state_data, @timeout}
+  end
+
+  def terminate({:shutdown, :timeout}, state_data) do
+    :ets.delete(:game_state, state_data.player1.name)
+    :ok
+  end
+
+  def terminate(_reason, _state), do: :ok
+
   def set_islands(game, player) when player in @players,
     do: GenServer.call(game, {:set_islands, player})
 
@@ -102,7 +119,10 @@ defmodule IslandsEngine.Game do
 
   defp update_rules(state_data, rules), do: %{state_data | rules: rules}
 
-  defp reply_success(state_data, reply), do: {:reply, reply, state_data, @timeout}
+  defp reply_success(state_data, reply) do
+    :ets.insert(:game_state, {state_data.player1.name, state_data})
+    {:reply, reply, state_data, @timeout}
+  end
 
   def position_island(game, player, key, row, col) when player in @players,
     do: GenServer.call(game, {:position_island, player, key, row, col})
@@ -119,5 +139,11 @@ defmodule IslandsEngine.Game do
     update_in(state_data[player_key].guesses, fn guesses ->
       Guesses.add(guesses, hit_or_miss, coordinate)
     end)
+  end
+
+  defp fresh_state(name) do
+    player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
+    player2 = %{name: nil, board: Board.new(), guesses: Guesses.new()}
+    %{player1: player1, player2: player2, rules: %Rules{}}
   end
 end
--- lib/islands_engine/gamesupervisor.ex
+++ lib/islands_engine/gamesupervisor.ex
@@ -10,6 +10,7 @@ defmodule IslandsEngine.GameSupervisor do
   def start_game(name), do: Supervisor.start_child(__MODULE__, [name])
 
   def stop_game(name) do
+    :ets.delete(:game_state, name)
     Supervisor.terminate_child(__MODULE__, pid_from_name(name))
   end
 
